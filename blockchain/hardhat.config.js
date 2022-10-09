a186133d03039423fdefb7fc4a618dd8d42849d3
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-09 13:17:04 +0900

    ReactとEthereumをつなぐための事前準備

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: process.env.API_KEY,
+      accounts: [process.env.SECRET_KEY],
+    },
+  },
 };
