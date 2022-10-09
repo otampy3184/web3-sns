af1fa35c71c1ca16d6f508fc721adaee81337534
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-09 13:17:42 +0900

    React上からGoerliネットワーク上のコントラクトと全投稿を取得する機能

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers"
+import abi from "./abi/Web3SNS.json";
 
 function App() {
-  //
   const [currentAccount, setCurrentAccount] = useState("");
   const [tweetValue, setTweetValue] = useState("");
   const [allTweets, setAllTweets] = useState("");
+
+  const contractAddress = "0x0eDd66F66aBF49655C58FB248E08867E1b0220B6";
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -52,6 +55,70 @@ function App() {
       console.log(error);
     }
   };
+
+  const getAllPosts = async() => {
+    const { ethereum } = window;
+    try {
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const web3SNSContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const posts = await web3SNSContract.getAllPosts();
+
+        const postsCleaned = posts.map((post) => {
+          return {
+            address: post.from,
+            timestamp: post.timestamp,
+            message: post.message,
+          };
+        })
+        setAllTweets(postsCleaned);
+      } else {
+        console.log("Ethereum object not found");
+      }
+    } catch(error){
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    let web3SNSContract;
+
+    const onNewPost = (from, message, timestamp, likes) => {
+      console.log("NewPost", from, message, timestamp, likes);
+      setAllTweets((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          message: message,
+          timestamp: new Date(timestamp * 1000),
+          likes: likes,
+        },
+      ]);
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      
+      web3SNSContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      )
+      web3SNSContract.on("NewPost", onNewPost);
+    }
+    return () => {
+      if(web3SNSContract) {
+        web3SNSContract.off("NewPost", onNewPost);
+      }
+    };
+  }, []);
 
   return (
     <div className="App">
